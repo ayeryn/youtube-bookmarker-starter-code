5af25c986c31de55c9abbc4dc30f5c57a8591c77
ayeryn <erynli@utexas.edu>
2024-04-18 17:28:09 -0500

    typo

--- background.js
+++ background.js
@@ -4,12 +4,13 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     // https://www.youtube.com/watch?v=UJ7QhP2_0-4
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
+    // console.log(urlParameters);
 
     // extension components can talk to each other
     // send the tabId to contentScript.js
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
-      videoId: urlParameters.get("v");
+      videoId: urlParameters.get("v"),
     });
   }
 });
