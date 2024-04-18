094ea29d2886e350faff72aa048cb61247b819a3
ayeryn <erynli@utexas.edu>
2024-04-18 17:26:21 -0500

    init

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    // UTB URL example:
+    // https://www.youtube.com/watch?v=UJ7QhP2_0-4
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    // extension components can talk to each other
+    // send the tabId to contentScript.js
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v");
+    });
+  }
+});
