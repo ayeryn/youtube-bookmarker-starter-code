b8aaf02824225d3c0232483261cf738f253277fd
ayeryn <erynli@utexas.edu>
2024-04-23 11:00:33 -0500

    remove log statements

--- background.js
+++ background.js
@@ -1,11 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  // console.log("hello world");
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     // UTB URL example:
     // https://www.youtube.com/watch?v=UJ7QhP2_0-4
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
-    console.log(urlParameters);
 
     // extension components can talk to each other
     // send to tab(tabId) to contentScript.js
