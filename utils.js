2fbbd70faeac4808af1446c141b1f4c837fc54fd
ayeryn <erynli@utexas.edu>
2024-04-23 11:18:56 -0500

    add comments

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
-// Get the current or most recently focused tab on chrome
-// code from chrome API doc
+/**
+ * Retrieves the URL of the active tab.
+ * @returns {Promise<chrome.tabs.Tab>} A promise that resolves to the active tab.
+ * code from chrome API doc
+ */
 export async function getActiveTabURL() {
   let queryOptions = { active: true, lastFocusedWindow: true }; // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
